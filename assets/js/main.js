81b747e37a3b7d939591679e584597b9ce2a6aeb
rainswirl <whatever>
2023-11-06 21:32:26 +0100

    Updates for static bottom text

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,25 +255,32 @@
     });
   });
 
-  window.addEventListener("scroll", positionParagraph, true);
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    let textElement = document.querySelector(".moving-text");
+    let offset = (scrollPosition / -7) + "px";
+    textElement.style.transform = "translateX(" + offset + ")";
+  });
 
-    // Function to calculate and update the position of the paragraph
-    function positionParagraph() {
-      // Get the image by class name
-      var img = document.querySelector('.background4');
-      // Get the paragraph by class name
-      var p = document.querySelector('.footer-line');
-      
-      // Calculate the position of the paragraph
-      var rect = img.getBoundingClientRect();
-      p.style.top = `${rect.bottom - p.offsetHeight}px`; // Align bottom edge with the bottom of the image
-      p.style.zIndex = 100000;
-    }
-  
-    // Run the function on DOM content load
-    document.addEventListener('DOMContentLoaded', positionParagraph, true);
-  
-    // Optional: If you need the paragraph to remain positioned correctly when the window is resized
-    window.addEventListener('resize', positionParagraph, true);
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    let textElement = document.querySelector(".moving-text1");
+    let offset = (scrollPosition / -40) + "px";
+    textElement.style.transform = "translateX(" + offset + ")";
+  });
+
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    let textElement = document.querySelector(".moving-text2");
+    let offset = (scrollPosition / -65) + "px";
+    textElement.style.transform = "translateX(" + offset + ")";
+  });
+
+  window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    let textElement = document.querySelector(".moving-text3");
+    let offset = (scrollPosition / -5) + "px";
+    textElement.style.transform = "translateY(" + offset + ")";
+  });
 
 })()
